3580483a8796ab70df418654473f9860027f61c1
Alexandr Kondratev <ak@cnord.ru>
2019-01-15 14:18:04 +0300

    Чистка game.py, возврат старых стратегий

--- demo/drones/reaper_drone.py
+++ demo/drones/reaper_drone.py
@@ -4,6 +4,7 @@
 import math
 import time
 import sys
+import random
 from operator import mul
 from robogame_engine import GameObject
 from robogame_engine.theme import theme
@@ -12,6 +13,7 @@ from robogame_engine.theme import theme
 from robogame_engine.geometry import Point, Vector, normalise_angle
 
 from astrobox.units import (MotherShip, Asteroid, DroneUnit)
+from astrobox.guns import Projectile
 from astrobox.cargo import CargoTransition
 
 from demo.drones.drone_unit_with_strategies import DroneUnitWithStrategies
@@ -36,6 +38,7 @@ class ReaperState(object):
     def __init__(self, strategy):
         assert(strategy is not None)
         self._strategy = strategy
+        self._ttl = 0
 
     @property
     def strategy(self):
@@ -53,19 +56,35 @@ class ReaperState(object):
         return None
 
     def game_step(self):
-        pass
+        self._ttl = self._ttl + 1
 
 class ReaperStateIdle(ReaperState):
     def __init__(self, strategy):
-        super(ReaperStateIdle, self).__init__(strategy);
+        super(self.__class__, self).__init__(strategy);
 
     def make_transition(self):
-        if len([a for a in self.scene.asteroids if a.cargo.payload>0]) > 0:
-            return ReaperStateHarvest
+        if not self.unit.is_alive:
+            return ReaperStateDead
         if self.unit.have_gun:
-            raise Exception("Guns have not implemented yet")
-        if len([a for a in self.scene.asteroids if a.cargo.payload>0]) == 0:
+            #raise Exception("Guns have not implemented yet")
+            pass
+        if self.unit.health < 0.6 and self.unit.distance_to(self.unit.mothership()) > theme.MOTHERSHIP_HEALING_DISTANCE:
+            return ReaperStateRunout
+        sources = self.scene.asteroids
+        sources = sources + [m for m in self.unit.scene.motherships if not m.is_alive and m.team != self.unit.team]
+        sources = sources + [d for d in self.unit.scene.drones if not d.is_alive]
+        has_sources = len([s for s in sources if s.cargo.payload>0]) > 0
+        if self.unit.cargo.fullness < 0.5:
+            if has_sources:
+                return ReaperStateHarvest
+        if not self.strategy.unit.cargo.is_empty:
             return ReaperStateUnload
+        #if not has_sources:
+        #    return ReaperStateUnload
+        return self.__class__
+
+class ReaperStateDead(ReaperState):
+    def make_transition(self):
         return self.__class__
 
 class ReaperStateUnload(ReaperState):
@@ -85,6 +104,7 @@ class ReaperStateUnload(ReaperState):
         return self.__class__
 
     def game_step(self):
+        super(self.__class__, self).game_step()
         if self._target is None:
             target = self.strategy.getUnloadTarget()
             if target is None:
@@ -117,7 +137,11 @@ class ReaperStateHarvest(ReaperState):
             return ReaperStateIdle
         if self._transition and self._transition.is_finished:
             return ReaperStateUnload
-        if len([a for a in self.scene.asteroids if a.cargo.payload>0])==0:
+        sources = self.scene.asteroids
+        sources = sources + [m for m in self.unit.scene.motherships if not m.is_alive and m.team != self.unit.team]
+        sources = sources + [d for d in self.unit.scene.drones if not d.is_alive]
+        has_sources = len([s for s in sources if s.cargo.payload>0]) > 0
+        if not has_sources:
             if self.unit.cargo.is_empty:
                 return ReaperStateIdle
             else:
@@ -125,6 +149,7 @@ class ReaperStateHarvest(ReaperState):
         return self.__class__
 
     def game_step(self):
+        super(self.__class__, self).game_step()
         if self._target is None:
             target = self.strategy.getHarvestTarget()
             if target is not None:
@@ -159,6 +184,8 @@ class ReaperStateRunout(ReaperState):
     def __init__(self, strategy):
         super(ReaperStateRunout, self).__init__(strategy);
         self._target = None
+        self._directions = [-25, 25]
+        random.shuffle(self._directions)
 
     def make_transition(self):
         if self.unit.health > 0.75:
@@ -166,29 +193,61 @@ class ReaperStateRunout(ReaperState):
         return self.__class__
 
     def game_step(self):
+        # FIXME: when stuck on borders
         if self._target is None:
-            target = self.unit.mothership()
+            v = Vector.from_points(self.unit.coord, self.unit.mothership().coord)
+            nextdir = self._directions.pop(0)
+            self._directions.append(nextdir)
+            v = v.from_direction(v.direction + nextdir, max(125, min(225, v.module*0.50)))
+            target = self.unit.coord + v
+            target.x = max(0, min(theme.FIELD_WIDTH, target.x))
+            target.y = max(0, min(theme.FIELD_HEIGHT, target.y))
+            #target = self.unit.mothership()
             if target is not None:
                 self._target = getPointOnWayTo(target, theme.CARGO_TRANSITION_DISTANCE * 0.9)
                 self.unit.move_at(self._target.copy())
             self.unit.move_at(self._target)
+        elif self.unit.distance_to(self._target) <= 1.0:
+            self._target = None
 
 class Dijkstra:
-    def __init__(self, unit, points):
+    def __init__(self, unit, points=None):
         self._unit = unit
-        self._points = points
-        self._weights = [[0.0 for _ in range(len(points))] for _ in range(len(points))]
+        self._points = points if points else []
+        self._weights = [[0.0 for _ in range(len(self._points))] for _ in range(len(self._points))]
+        #self._weights_range = [0.0, 0.0]
 
     @staticmethod
     def maxint():
         return sys.maxsize
 
+    @property
+    def points(self):
+        return self._points
+
+    @property
+    def weights(self):
+        return self._weights
+
+    def update_units(self, func=None):
+        if func is None:
+            func = lambda a: True
+        units = [self._unit.mothership(),]
+        units = units + [a for a in self._unit.scene.asteroids if func(a)]
+        units = units + [m for m in self._unit.scene.motherships if not m.is_alive and m.team != self._unit.team and func(m)]
+        units = units + [d for d in self._unit.scene.drones if not d.is_alive and func(d)]
+        weights = [[0.0 for _ in range(len(units))] for _ in range(len(units))]
+        self._weights, self._points = weights, units
+
     def get_closest(self, unit):
+        if not self._unit.is_alive:
+            return
         uclosest = self._points[0]
         dclosest = self._points[0].distance_to(unit)
         for u in self._points:
-            if u.distance_to(unit) < dclosest:
-                dclosest = u.distance_to(unit)
+            chkdist = unit.distance_to(u)
+            if chkdist < dclosest:
+                dclosest = chkdist
                 uclosest = u
         return uclosest
 
@@ -196,8 +255,11 @@ class Dijkstra:
         return [self._points[n] for n in indexes]
 
     def calc_weights(self, func=None):
+        if not self._unit.is_alive:
+            return
         if func is None:
             func = lambda a,b: float(a.distance_to(b))
+        #self._weights_range = [self.maxint, 0.0]
         for f,a in enumerate(self._points):
             for t,b in enumerate(self._points):
                 if f == t:
@@ -205,10 +267,18 @@ class Dijkstra:
                 else:
                     d = float(func(a, b))
                     self._weights[f][t] = d
+                    #if self._weights_range[0]>d:
+                    #    self._weights_range[0]=d
+                    #if self._weights_range[1]<d:
+                    #    self._weights_range[1]=d
             #print("%s "%(self._unit.id) + ",".join(["%8.2f"%d for d in self._weights[k]]))
             #print("")
 
     def find_path(self, pt_from, pt_to, as_objects=False, info=None):
+        if not self._unit.is_alive:
+            return
+        if pt_from not in self._points or pt_to not in self._points:
+            print(pt_from, pt_to, self._points)
         fi = self._points.index(pt_from)
         fo = self._points.index(pt_to)
         if fi == fo:
@@ -275,6 +345,7 @@ class ReaperStrategy(Strategy):
             self._drones = []
 
     _data = {}
+    _test_unit = None
 
     @property
     def data(self):
@@ -287,71 +358,74 @@ class ReaperStrategy(Strategy):
         self.data._drones.append(self.unit);
 
         # PathFinder
-        units = [self.unit.mothership(),]
-        units = units + self.unit.scene.asteroids
         if self.unit.pathfind is None:
-            self.unit.pathfind = Dijkstra(self.unit, units[:])
+            self.unit.pathfind = Dijkstra(self.unit)
         if self.unit.pathfind_unload is None:
-            self.unit.pathfind_unload = Dijkstra(self.unit, units[:])
+            self.unit.pathfind_unload = Dijkstra(self.unit)
+        self.data._enemy_drones = [d for d in self.unit.scene.drones if d.team != self.unit.team]
 
     def getHarvestTarget(self):
+        self.unit.pathfind.update_units(func=lambda a: not a.cargo.is_empty)
         uclosest = self.unit.pathfind.get_closest(self.unit)
 
-        k = math.sqrt(theme.FIELD_HEIGHT*theme.FIELD_HEIGHT + theme.FIELD_HEIGHT*theme.FIELD_WIDTH)
-        # TODO: do better choice than a fattest asteroid
-        fat_ast = self.unit.scene.asteroids
-        fat_ast.sort(key=lambda a: a.cargo.payload, reverse=True)
-        fat_ast = fat_ast[0]
-
+        k = math.sqrt(theme.FIELD_HEIGHT*theme.FIELD_HEIGHT + theme.FIELD_WIDTH*theme.FIELD_WIDTH)
         def weight_func(a, b):
             if a.distance_to(b) > ReaperStrategy._distance_limit:
                 return float("inf")
             if b.cargo.fullness == 0.0:
                 return float("inf")
-            coef =   [k,                       1.0]
+            coef =   [0.5/k,                   1.0]
             values = [float(a.distance_to(b)), b.cargo.fullness]
             return sum(map(mul, coef, values))
         self.unit.pathfind.calc_weights(func=weight_func)
+        # TODO: do better choice than a fattest asteroid
+        fat_source = [p for p in self.unit.pathfind.points if p != self.unit.mothership()]
+        fat_source.sort(key=lambda a: a.cargo.payload/a.scene.max_elerium + self.unit.mothership().distance_to(a)/k, reverse=True)
+        if not fat_source:
+            return None
+        fat_source = fat_source[0]
+        self.unit.pathfind.weights
 
-        path = self.unit.pathfind.find_path(self.unit.mothership(), fat_ast, as_objects=True, info="harv")
+        path = self.unit.pathfind.find_path(self.unit.mothership(), fat_source, as_objects=True, info="harv")
+        if path is None:
+            return None
 
         pos = self.data._drones.index(self.unit)
         sz = len(path)
-        idx = (pos % (sz-1)) + 1 if sz>1 else 0
+        idx = min(sz, (pos % (sz-1)) + 1 if sz>1 else 0)
         #print("Team {}:{} #{},{} path[{}] target #{}: {}".format(self.unit.team, self.unit.id, pos, idx, "harv", idx, path[idx]))
         return path[idx]
 
     def getUnloadTarget(self):
         if len([a for a in self.unit.scene.asteroids if a.cargo.payload>0])==0:
             return self.unit.mothership()
+
+        self.unit.pathfind_unload.update_units(func=lambda u: u.cargo.fullness<0.7)
+
         k = math.sqrt(theme.FIELD_HEIGHT*theme.FIELD_HEIGHT + theme.FIELD_HEIGHT*theme.FIELD_WIDTH)
         def weight_func(a, b):
             if a.distance_to(b) > ReaperStrategy._distance_limit:
                 return float("inf")
+            dist = a.distance_to(b)
+            vdist = 0.0
+            if dist != 0.0:
+                vdist = float(self.unit.distance_to(self.unit.mothership()))/float(a.distance_to(b))
             coef =   [1.0, 1.0]
-            values = [float(self.unit.distance_to(self.unit.mothership()))/float(a.distance_to(b)), 1.0-b.cargo.fullness]
+            values = [vdist, 1.0-b.cargo.fullness]
             return sum(map(mul, coef, values))
+        uclosest = self.unit.pathfind_unload.get_closest(self.unit)
         self.unit.pathfind_unload.calc_weights(func=weight_func)
 
-        uclosest = self.unit.pathfind_unload.get_closest(self.unit)
         path_unload = self.unit.pathfind_unload.find_path(uclosest, self.unit.mothership(), as_objects=True, info="unld")
+        if path_unload is None:
+            return None
 
         pos = self.data._drones.index(self.unit)
         sz = len(path_unload)
-        idx = (pos % (sz-1)) + 1 if sz>1 else 0
+        # Возврат, проекция бинарного поиска в отношении path-finding
+        idx = min(len(path_unload)-1, int(len(path_unload)/2) + 1) if sz>1 else 0
         #print("Team {}:{} #{},{} path[{}] target #{}: {}".format(self.unit.team, self.unit.id, pos, idx, "unld", idx, path_unload[idx]))
         return path_unload[-idx]
-
-    def _find_closest_elerium(self, unit, units):
-        our_mothership = self.unit.mothership()
-        enemy_motherships = [m for m in self.unit.scene.motherships if m.id != our_mothership.id]
-        asteroids = []
-        for a in units:
-            dist_to_a = unit.distance_to(a)
-            safepoints = min([dist_to_a/m.distance_to(a) for m in enemy_motherships])
-            asteroids.append((safepoints, a))
-        asteroids.sort(key=lambda o: o[0])
-        return asteroids
 
     @property
     def is_finished(self):
@@ -361,8 +435,48 @@ class ReaperStrategy(Strategy):
     def fsm_state(self):
         return self.unit.fsm_state
 
+    def gun_fire(self):
+        if not self.unit.have_gun:
+            return
+        def is_on_straight(b):
+            v = Vector.from_points(self.unit.coord, b.coord)
+            v.rotate(-self.unit.direction)
+            if v.direction < 90.0:
+                a = v.direction/180.0*math.pi
+                limit = (DroneUnit.radius+Projectile.radius)
+                if self.unit.team == b.team:
+                    limit*=1.5
+                return math.fabs(self.unit.distance_to(b)*math.sin(a)) < limit
+            return False
+
+        if self._test_unit is None:
+            test = [d for d in self.unit.scene.drones if self.unit.team != d.team][0]
+            self._test_unit = [self.unit.id, test.id]
+
+        def target_drones():
+            for d in self.unit.scene.drones:
+                if not d.is_alive or d.id == self.unit.id:
+                    continue
+                if self.unit.distance_to(d) > theme.PROJECTILE_TTL*theme.PROJECTILE_SPEED*0.8:
+                    continue
+                if is_on_straight(d):
+                    yield d
+        targets = list(target_drones())
+        if targets:
+            if is_on_straight(self.unit.mothership()):
+                targets.append(self.unit.mothership())
+            targets.sort(key=lambda t: self.unit.distance_to(t))
+
+            #print("Team {}:{}\t{} {}".format(self.unit.team, self.unit.id, len(targets), len(teammates)))
+            #print("Team {}:{}\t{}".format(self.unit.team, self.unit.id, targets))
+            if self.unit.team != targets[0].team:
+                #print("Team {}:{} {}".format(self.unit.team, self.unit.id, targets))
+                self.unit.gun.shot([d for d in targets if d.team != self.unit.team][0])
+
     def game_step(self, **kwargs):
         super(ReaperStrategy, self).game_step(**kwargs)
+
+        self.gun_fire()
 
         newState = self.fsm_state.make_transition()
         if newState != self.fsm_state.__class__:
--- demo/game.py
+++ demo/game.py
@@ -6,24 +6,13 @@ print(os.environ["PYTHONPATH"])
 from robogame_engine.theme import theme
 from robogame_engine.geometry import Point, Vector
 
-#from astrobox.guns import PlasmaGun
 from astrobox.space_field import SpaceField
-from astrobox.units import DroneUnit #, Unit
-
-#from demo.drones.drone_unit_with_strategies import DroneUnitWithStrategies
-
-#from demo.drones.worker_drone import WorkerDrone
-#from demo.drones.greedy_drone import GreedyDrone
-#from demo.drones.hunter_drone import HunterDrone
-#from demo.drones.destroyer_drone import DestroyerDrone
 
 from demo.drones.reaper_drone import ReaperDrone
-
-class NoobDrone(ReaperDrone):
-    pass
-
-class NoobDroneFoo(ReaperDrone):
-    pass
+#from demo.drones.worker_drone import WorkerDrone
+from demo.drones.greedy_drone import GreedyDrone
+from demo.drones.hunter_drone import HunterDrone
+from demo.drones.destroyer_drone import DestroyerDrone
 
 if __name__ == '__main__':
     space_field = SpaceField(
@@ -34,11 +23,8 @@ if __name__ == '__main__':
     )
 
     teamA = [ReaperDrone() for _ in range(theme.TEAM_DRONES_COUNT)]
-    teamB = [NoobDrone() for _ in range(theme.TEAM_DRONES_COUNT)]
-    teamC = [NoobDroneFoo() for _ in range(theme.TEAM_DRONES_COUNT)]
-    #teamA = [WorkerDrone()    for _ in range(theme.TEAM_DRONES_COUNT)]
-    #teamB = [GreedyDrone()    for _ in range(theme.TEAM_DRONES_COUNT)]
-    #teamC = [HunterDrone()    for _ in range(theme.TEAM_DRONES_COUNT)]
-    #teamD = [DestroyerDrone() for _ in range(theme.TEAM_DRONES_COUNT)]
+    teamB = [GreedyDrone()    for _ in range(theme.TEAM_DRONES_COUNT)]
+    teamC = [HunterDrone()    for _ in range(theme.TEAM_DRONES_COUNT)]
+    teamD = [DestroyerDrone() for _ in range(theme.TEAM_DRONES_COUNT)]
 
     space_field.go()
