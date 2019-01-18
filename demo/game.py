a0b286fdd5d6228d55fa5963e7910c93bf829e9d
wad <suguby@gmail.com>
2019-01-18 12:54:07 +0300

    DRONES_CAN_FIGHT = False by default

--- demo/game.py
+++ demo/game.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
-import random
 
 import os
+
 print(os.environ["PYTHONPATH"])
 from robogame_engine.theme import theme
-from robogame_engine.geometry import Point, Vector
 
 from astrobox.space_field import SpaceField
 
 from demo.drones.reaper_drone import ReaperDrone
-#from demo.drones.worker_drone import WorkerDrone
+# from demo.drones.worker_drone import WorkerDrone
 from demo.drones.greedy_drone import GreedyDrone
 from demo.drones.hunter_drone import HunterDrone
-from demo.drones.destroyer_drone import DestroyerDrone
+# from demo.drones.destroyer_drone import DestroyerDrone
 
 if __name__ == '__main__':
     space_field = SpaceField(
@@ -23,8 +22,8 @@ if __name__ == '__main__':
     )
 
     teamA = [ReaperDrone() for _ in range(theme.TEAM_DRONES_COUNT)]
-    teamB = [GreedyDrone()    for _ in range(theme.TEAM_DRONES_COUNT)]
-    teamC = [HunterDrone()    for _ in range(theme.TEAM_DRONES_COUNT)]
+    teamB = [GreedyDrone() for _ in range(theme.TEAM_DRONES_COUNT)]
+    teamC = [HunterDrone() for _ in range(theme.TEAM_DRONES_COUNT)]
     teamD = [DestroyerDrone() for _ in range(theme.TEAM_DRONES_COUNT)]
 
     space_field.go()
